2ce0bf5e6c0e1a1171e1552ee68de0c4b22c6cb1
emanuele <manuelfavaa@gmail.com>
2025-05-01 18:23:28 +0200

    chores(treefolder): fixed treefolder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,15 @@ import { Esercizio2Component } from './pages/esercizio-2/esercizio-2.component';
 import { Esercizio3Component } from './pages/esercizio-3/esercizio-3.component';
 import { Esercizio4Component } from './pages/esercizio-4/esercizio-4.component';
 import { Esercizio5Component } from './pages/esercizio-5/esercizio-5.component';
-import { RoutingParamsComponent } from './components/components-esercizi/routing-params/routing-params.component';
-import { QueryParamsComponent } from './components/components-esercizi/query-params/query-params.component';
+import { RoutingParamsComponent } from './components/components-esercizi-recenti/routing-params/routing-params.component';
+import { QueryParamsComponent } from './components/components-esercizi-recenti/query-params/query-params.component';
 import { FormControl, FormGroup } from '@angular/forms';
-import { FormControllerComponent } from './components/components-esercizi/form-controller/form-controller.component';
-import { FormValidatorComponent } from './components/components-esercizi/form-validator/form-validator.component';
-import { FormGroupComponent } from './components/components-esercizi/form-group/form-group.component';
-import { FormQueriesComponent } from './components/components-esercizi/form-queries/form-queries.component';
-import { SubmitReciverComponent } from './components/components-esercizi/submit-reciver/submit-reciver.component';
-import { FormSignUpComponent } from './components/components-esercizi/form-sign-up/form-sign-up.component';
+import { FormControllerComponent } from './components/components-esercizi-recenti/form-controller/form-controller.component';
+import { FormValidatorComponent } from './components/components-esercizi-recenti/form-validator/form-validator.component';
+import { FormGroupComponent } from './components/components-esercizi-recenti/form-group/form-group.component';
+import { FormQueriesComponent } from './components/components-esercizi-recenti/form-queries/form-queries.component';
+import { SubmitReciverComponent } from './components/components-esercizi-recenti/submit-reciver/submit-reciver.component';
+import { FormSignUpComponent } from './components/components-esercizi-recenti/form-sign-up/form-sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
